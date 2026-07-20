98eddb573fed65e6267823f0b6db98f0c3e96fea
rd-luttu <anandbhari123@gmail.com>
2026-07-20 14:50:15 +0530

    Fix Vercel 404 by outputting index.html to dist root

--- scripts/prerender.js
+++ scripts/prerender.js
@@ -5,12 +5,12 @@ import { fileURLToPath, pathToFileURL } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const toAbsolute = (p) => path.resolve(__dirname, p);
 
-const template = fs.readFileSync(toAbsolute('../dist/static/index.html'), 'utf-8');
+const template = fs.readFileSync(toAbsolute('../dist/index.html'), 'utf-8');
 const serverEntryPath = toAbsolute('../dist/server/entry-server.js');
 const { render } = await import(pathToFileURL(serverEntryPath).href);
 
 const { html } = render();
 const htmlWithApp = template.replace('<!--app-html-->', html);
 
-fs.writeFileSync(toAbsolute('../dist/static/index.html'), htmlWithApp);
+fs.writeFileSync(toAbsolute('../dist/index.html'), htmlWithApp);
 console.log('Pre-render complete.');
